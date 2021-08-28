25d1f1e437ea3beeff047f051c42f752505a71be
NaveenKumarS <naveenkumar.s@siemens.com>
2021-08-28 21:15:00 +0530

    init

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var cors = require('cors')
 var app = express();
-// var cors = require('cors')
+
+
 var port = process.env.PORT || 3000;
-// app.use(cors())
+ app.use(cors())
+app.get('/', function (req, res) {
+    console.log("recevuied");
+    res.send('Sample Node.js App');
+});
 app.get('/user', function (req, res) {
     console.log("recevuied");
     res.send('Sample Node.js App');
